d5bb5a0474275878f9980382adf4bb1b435a8919
gsh <kevin@yusi.studio>
2019-03-26 15:24:07 +0800

    1

---
+++ java/src.design/prototype/Squre.java
@@ -0,0 +1,14 @@
+package prototype;
+
+public class Squre extends Shape {
+
+    public Squre(){
+        type = "Squre";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("Squre draw");
+    }
+
+}
---
+++ java/src.design/prototype/main.java
@@ -0,0 +1,14 @@
+package prototype;
+
+public class main {
+
+    public static void main(String[] args) {
+        ShapeCache.loadCache();
+        Shape clonedShape = (Shape) ShapeCache.getShap("1");
+        System.out.println("Shape : " + clonedShape.getType());
+
+        Shape clonedShape2 = (Shape) ShapeCache.getShap("2");
+        System.out.println("Shape : " + clonedShape2.getType());
+
+    }
+}
